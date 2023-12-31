da75be4ac750a0b655ccfc1b4de69d0bb157405b
Jaakko Heusala <jhh@hg.fi>
2024-01-01 00:44:36 +0200

    Fixed IDE type errors

--- jest/functions/flushPromises.ts
+++ jest/functions/flushPromises.ts
@@ -1,3 +1,5 @@
 // Copyright (c) 2023. Heusala Group Oy <info@heusalagroup.fi>. All rights reserved.
 
-export const flushPromises = () => new Promise(jest.requireActual("timers").setImmediate);
+import { jest } from '@jest/globals';
+
+export const flushPromises = () => new Promise((jest as any).requireActual("timers").setImmediate);
